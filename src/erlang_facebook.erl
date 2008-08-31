0d449fad7cb6967d21bbfdf61767d239c203b116
Nick Gerakines <nick@gerakines.net>
2008-08-31 08:22:13 -0700

    Adding fix for arg parsing

--- src/erlang_facebook.erl
+++ src/erlang_facebook.erl
@@ -142,8 +142,8 @@ from_args("fb_sig", Args) ->
         {value, {"fb_sig", Value}} -> Value;
         _ -> none
     end;
-from_args(Key, Args) ->
-    case lists:keysearch(Key, 1, Args) of
+from_args("fb_sig_" ++ Key, Args) ->
+    case lists:keysearch("fb_sig_" ++ Key, 1, Args) of
         {value, {_, Value}} -> Value;
         _ -> none
     end.
